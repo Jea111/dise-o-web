3e1f958b8110257cb6c3be9a5c028762592dffc7
jea111 <laliendra42@gmail.com>
2025-03-10 12:18:13 -0500

    Clase diseño web poli

--- JS/contact.js
+++ JS/contact.js
@@ -1,3 +1,17 @@
 function btnData() {
-  swal("RECIBIENDO DATOS", "GRACIAS POR COMUNICARNOS!", "success");
+  swal({
+    title: "ENVIA LOS DATOS SOLO SI CONFIAS EN ESTA PÁGINA",
+    text: "ESTAREMOS EN CONTACTO CONTIGO \n LO MAS PRONTO POSIBLE!",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  }).then((willDelete) => {
+    if (willDelete) {
+      swal("GRACIAS POR ENVIARNOS TU INFORMACIÓN!", {
+        icon: "success",
+      });
+    } else {
+      swal("ES UNA PENA, TE ESPERAMOS PRONTO");
+    }
+  });
 }
