430506b42c635df3e0f35e400a1bf3de0189f13d
jea111 <laliendra42@gmail.com>
2025-03-13 20:27:14 -0500

    Clase diseño web poli

--- JS/contact.js
+++ JS/contact.js
@@ -1,17 +1,28 @@
 function btnData() {
-  swal({
-    title: "ENVIA LOS DATOS SOLO SI CONFIAS EN ESTA PÁGINA",
-    text: "ESTAREMOS EN CONTACTO CONTIGO \n LO MAS PRONTO POSIBLE!",
-    icon: "warning",
-    buttons: true,
-    dangerMode: true,
-  }).then((willDelete) => {
-    if (willDelete) {
-      swal("GRACIAS POR ENVIARNOS TU INFORMACIÓN!", {
-        icon: "success",
+  let buttons = document.querySelector(".btn");
+
+  // Verificar si el botón existe antes de añadir el evento
+  if (buttons) {
+    buttons.addEventListener("click", function () {
+      swal({
+        title: "ENVIA LOS DATOS SOLO SI CONFIAS EN ESTA PÁGINA",
+        text: "ESTAREMOS EN CONTACTO CONTIGO \n LO MAS PRONTO POSIBLE!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      }).then((willDelete) => {
+        if (willDelete) {
+          swal("GRACIAS POR ENVIARNOS TU INFORMACIÓN!", {
+            icon: "success",
+          });
+        } else {
+          swal("ES UNA PENA, TE ESPERAMOS PRONTO");
+        }
       });
-    } else {
-      swal("ES UNA PENA, TE ESPERAMOS PRONTO");
-    }
-  });
+    });
+  } else {
+    console.log("No se encontró el botón");
+  }
 }
+
+btnData();
--- JS/gallery.js
+++ JS/gallery.js
@@ -1,3 +1,43 @@
-function btn() {
-  swal("¡STOCK DISPONIBLE!", "CONFIRMANDO COMPRA", "success");
+function botonCompra() {
+  let buttons = document.querySelectorAll(".btn"); // Seleccionas todos los  botones con la clase 'btn'
+
+  // Verificamos si se encuentra el botón antes de asignarle el evento
+  // //asignar a todos los botones con el (.length > 0)
+  if (buttons.length > 0) {
+    // Utilizamos forEach para iteratuar sobre cada botón encontrado y agregarle un evento click.
+    buttons.forEach((button) => {
+      // Iteramos sobre cada botón
+      button.addEventListener("click", function () {
+        swal("¡STOCK DISPONIBLE!", "CONFIRMANDO COMPRA", "success");
+      });
+    });
+  } else {
+    console.log("No se encontró el botón");
+  }
 }
+
+botonCompra();
+
+function btnSearch() {
+  let search = document.querySelector(".btnSearch");
+
+  search.addEventListener("click", function () {
+    swal("producto encontrado", "confima la compra", "success");
+  });
+}
+btnSearch();
+
+// function busqueda() {
+//   let input = document.querySelector(".search-container"); // Seleccionamos el campo de texto del input
+//   input.addEventListener("input", function () {
+//     let searchValue = input.value.toLowerCase(); // Obtenemos el valor del input, pasamos a minúsculas para la comparación
+
+//     if (searchValue === "adidas") {
+//       swal("Producto encontrado", "Confirma la compra", "success");
+//     } else if (searchValue === "nike") {
+//       swal("Producto no disponible");
+//     } else {
+//       swal("Producto no encontrado");
+//     }
+//   });
+// }
